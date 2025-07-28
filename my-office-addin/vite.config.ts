7b6f4bfc667d33beca9e0fee96bfbc8bc9294055
dishizawa <dishizawa2001@gmail.com>
2025-07-28 22:42:12 +0900

    fix: :bug: viteがsrc/を見つけれないバグを修正

--- my-office-addin/vite.config.ts
+++ my-office-addin/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import officeAddin from 'vite-plugin-office-addin';
-import basicSsl from '@vitejs/plugin-basic-ssl'
-
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,11 +13,11 @@ export default defineConfig({
     officeAddin({
       devUrl: 'https://localhost:1234',
       prodUrl: 'https://<your>.github.io',
-      path: 'manifest.xml'
-    })
+      path: 'manifest.xml',
+    }),
   ],
   server: {
-    port: 1234
+    port: 1234,
   },
   build: {
     outDir: 'dist',
@@ -26,11 +25,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         // taskpane.html をビルドのエントリに追加
-        taskpane: path.resolve(__dirname, 'taskpane.html')
+        taskpane: path.resolve(__dirname, 'taskpane.html'),
       },
-    }
+    },
   },
   resolve: {
-  alias: { '@': path.resolve(__dirname, 'src') }
-  }
-})
+    alias: { src: path.resolve(__dirname, 'src') },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+});
