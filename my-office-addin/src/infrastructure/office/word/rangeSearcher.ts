35c809ba111d3163df163b4dcb40bb4cd291e9be
dishizawa <dishizawa2001@gmail.com>
2025-10-20 05:03:25 +0900

    fix: :bug: アルファベット以外が隣接するケースが対象外になるバグを修正

--- my-office-addin/src/infrastructure/office/word/rangeSearcher.ts
+++ my-office-addin/src/infrastructure/office/word/rangeSearcher.ts
@@ -14,9 +14,9 @@ export class EnglishSearcher implements IRangeSearcher {
   run(_mapping: Mapping, body: Word.Body): Word.RangeCollection {
     // 連続したアルファベット(大文字・小文字)のブロックを検索
     // 全角英字（Ａ-Ｚ、ａ-ｚ）も含める
-   return body.search('[A-Za-zＡ-Ｚａ-ｚ]{1,}', {
+    return body.search('<[A-Za-zＡ-Ｚａ-ｚ]{1,}>', {
       matchCase: false,
-      matchWholeWord: true,
+      matchWholeWord: false,
       matchWildcards: true,
     });
   }
